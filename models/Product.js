6b3be52f1e8e256de8fbef7c11e965188e821941
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-06-01 22:34:19 +0700

    [Es6]: Create Product template

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+class Product {
+  constructor(
+    id,
+    name,
+    price,
+    screen,
+    backCamera,
+    frontCamera,
+    img,
+    desc,
+    type,
+    fontCamera,
+    image,
+    inventory,
+    rating
+  ) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.screen = screen;
+    this.backCamera = backCamera;
+    this.frontCamera = frontCamera;
+    this.img = img;
+    this.desc = desc;
+    this.type = type;
+    this.fontCamera = fontCamera;
+    this.image = image;
+    this.inventory = inventory;
+    this.rating = rating;
+  }
+}
